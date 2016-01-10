3c8671018b7d59ede26763e3e4174aece7b49b19
Michael Kutý <6du1ro.n@gmail.com>
2016-01-10 09:42:40 +0100

    Move widget imports to models.py

--- leonardo_counters/__init__.py
+++ leonardo_counters/__init__.py
@@ -1,15 +1,14 @@
 
 from django.apps import AppConfig
 
-from .widget.countdown.models import CountdownWidget
-from .widget.counter.models import CounterWidget
 
 default_app_config = 'leonardo_counters.Config'
 
 
 LEONARDO_APPS = ['leonardo_counters']
 
-LEONARDO_WIDGETS = [CounterWidget, CountdownWidget]
+LEONARDO_WIDGETS = ['leonardo_counters.models.CounterWidget',
+                    'leonardo_counters.models.CountdownWidget']
 
 LEONARDO_JS_FILES = [
     'js/lib/jquery.counter.js',
