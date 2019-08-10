0ca241f49f0f2b3c377a4e64f4e112174aadd6e2
Dominik Resl <nikicresl@gmail.com>
2019-08-10 15:31:56 +0200

    Update for new deploy

--- leonardo_counters/__init__.py
+++ leonardo_counters/__init__.py
@@ -1,20 +1,24 @@
 
 from django.apps import AppConfig
 
+try:
+    from local_settings import APPS
+except ImportError:
+    pass
 
 default_app_config = 'leonardo_counters.Config'
 
+if 'leonardo_counters' in APPS:
+    LEONARDO_APPS = ['leonardo_counters']
 
-LEONARDO_APPS = ['leonardo_counters']
+    LEONARDO_WIDGETS = ['leonardo_counters.models.CounterWidget',
+                        'leonardo_counters.models.CountdownWidget']
 
-LEONARDO_WIDGETS = ['leonardo_counters.models.CounterWidget',
-                    'leonardo_counters.models.CountdownWidget']
-
-LEONARDO_JS_FILES = [
-    'js/lib/jquery.counter.js',
-    'js/lib/jquery.appear.js',
-]
-LEONARDO_OPTGROUP = "Counters"
+    LEONARDO_JS_FILES = [
+        'js/lib/jquery.counter.js',
+        'js/lib/jquery.appear.js',
+    ]
+    LEONARDO_OPTGROUP = "Counters"
 
 
 class Config(AppConfig):
